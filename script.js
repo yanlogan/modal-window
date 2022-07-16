c81eb3e82cf345364bbb40ac6dec5b9db1b2fa2a
Yan Logan <yanlogans@gmail.com>
2022-07-16 19:42:23 +0300

    feat: add opening and closing the modal

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const btns = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', openModal);
+}
+
+closeBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
